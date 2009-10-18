27e0db602f5ef9c4ca01de2472bb3cfcc7b70b40
fredrikbk <fredrikbk@gmail.com>
2009-10-17 23:23:33 -0500

    Implementing a check for whether a field is already initialized in a constructor, in which case the refactoring doesn't add an initializer

--- src/edu/uiuc/immutability/AccessAnalyzerForImmutability.java
+++ src/edu/uiuc/immutability/AccessAnalyzerForImmutability.java
@@ -4,13 +4,20 @@ import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
 
+import org.eclipse.core.runtime.CoreException;
 import org.eclipse.jdt.core.Flags;
 import org.eclipse.jdt.core.ICompilationUnit;
+import org.eclipse.jdt.core.IField;
+import org.eclipse.jdt.core.IJavaElement;
+import org.eclipse.jdt.core.IMethod;
+import org.eclipse.jdt.core.IType;
+import org.eclipse.jdt.core.JavaModelException;
 import org.eclipse.jdt.core.dom.AST;
 import org.eclipse.jdt.core.dom.ASTNode;
 import org.eclipse.jdt.core.dom.ASTVisitor;
 import org.eclipse.jdt.core.dom.AnnotationTypeDeclaration;
 import org.eclipse.jdt.core.dom.FieldDeclaration;
+import org.eclipse.jdt.core.dom.MethodDeclaration;
 import org.eclipse.jdt.core.dom.VariableDeclarationFragment;
 import org.eclipse.jdt.core.dom.NumberLiteral;
 import org.eclipse.jdt.core.dom.CharacterLiteral;
@@ -22,6 +29,13 @@ import org.eclipse.jdt.core.dom.Modifier;
 import org.eclipse.jdt.core.dom.TypeDeclaration;
 import org.eclipse.jdt.core.dom.Modifier.ModifierKeyword;
 import org.eclipse.jdt.core.dom.rewrite.ASTRewrite;
+import org.eclipse.jdt.core.search.IJavaSearchConstants;
+import org.eclipse.jdt.core.search.IJavaSearchScope;
+import org.eclipse.jdt.core.search.SearchEngine;
+import org.eclipse.jdt.core.search.SearchMatch;
+import org.eclipse.jdt.core.search.SearchParticipant;
+import org.eclipse.jdt.core.search.SearchPattern;
+import org.eclipse.jdt.core.search.SearchRequestor;
 import org.eclipse.jdt.internal.corext.dom.ASTNodes;
 import org.eclipse.jdt.internal.corext.dom.Bindings;
 import org.eclipse.jdt.internal.corext.dom.ModifierRewrite;
@@ -31,10 +45,11 @@ import org.eclipse.text.edits.TextEditGroup;
 public class AccessAnalyzerForImmutability extends ASTVisitor {
 
 	private RefactoringStatus status;
-	private List<TextEditGroup> groupDescriptions, blah;
+	private List<TextEditGroup> groupDescriptions;
 	private final MakeImmutableRefactoring refactoring;
 	private final ASTRewrite rewriter;
 	private final AST astRoot;
+	private final ICompilationUnit unit;
 	
 	public AccessAnalyzerForImmutability(
 			MakeImmutableRefactoring makeImmutableRefactoring,
@@ -42,6 +57,7 @@ public class AccessAnalyzerForImmutability extends ASTVisitor {
 		this.refactoring = makeImmutableRefactoring;
 		this.rewriter = rewriter;
 		this.astRoot = rewriter.getAST();
+		this.unit = unit;
 		status = new RefactoringStatus();
 		groupDescriptions = new ArrayList<TextEditGroup>();
 	}
@@ -59,58 +75,109 @@ public class AccessAnalyzerForImmutability extends ASTVisitor {
 				groupDescriptions.add(gd);
 			}
 			
-			// Add initializers to the fragments that do not have them
+			// Add initializers to the fragments that do not have them as they are required for final variables
 			Type fieldDeclType = fieldDecl.getType(); 
 			List fragments = fieldDecl.fragments();
 			for (Object obj : fragments) {
+				
 				VariableDeclarationFragment frag = (VariableDeclarationFragment)obj;
 				if (frag != null && frag.getInitializer() == null) {
-					VariableDeclarationFragment newFrag = (VariableDeclarationFragment)ASTNode.copySubtree(frag.getAST(), frag);
-					Expression initializer = null;
-					if (fieldDeclType instanceof PrimitiveType) {
-						PrimitiveType primType = (PrimitiveType)fieldDeclType;
-						
-						if (primType.getPrimitiveTypeCode() == PrimitiveType.BOOLEAN) {
-							initializer = newFrag.getAST().newBooleanLiteral(false);
-						}
-						else if (primType.getPrimitiveTypeCode() == PrimitiveType.CHAR) {
-							CharacterLiteral charLit = newFrag.getAST().newCharacterLiteral(); 
-							charLit.setCharValue('\u0000');
+					VariableDeclarationFragment newFrag =
+							(VariableDeclarationFragment)ASTNode.copySubtree(frag.getAST(), frag);
+					
+					// Check whether the field is already initialized in a constructor in which case we can't
+					// initialize it a second time at the declaration point
+					// Get the class of the variable (The grandparent of a field variable must always be its class)
+					boolean isAlreadyInitialized = false;
+					TypeDeclaration parentClass = (TypeDeclaration)fieldDecl.getParent();
+					assert parentClass.isInterface() == false; //Interfaces can't have non-static fields
+					
+					// Go through all the constructors and check whether they initialize the variable
+					IType classTypeId = unit.getType(parentClass.getName().toString());
+					IField field = classTypeId.getField(frag.getName().toString());
+					
+					IMethod[] methods;
+					try {
+						methods = classTypeId.getMethods();						
+						for (IMethod method : methods) {
+							if (method.isConstructor()) {
+								SearchPattern pattern = SearchPattern.createPattern(field, IJavaSearchConstants.WRITE_ACCESSES);
+								SearchEngine engine = new SearchEngine();
+								SearchParticipant[] participants = new SearchParticipant[] { SearchEngine.getDefaultSearchParticipant() };
+								IJavaSearchScope scope = SearchEngine.createJavaSearchScope(new IJavaElement[] { method });
 								
-							initializer = charLit;
+								final List matches = new ArrayList();
+								SearchRequestor requestor = new SearchRequestor() {
+									public void acceptSearchMatch(SearchMatch match) {
+										matches.add(match);
+									}
+								};
+								
+								engine.search(pattern, participants, scope, requestor, null);
+								if (!matches.isEmpty()) {
+									// Skip over the add initializer step as this variable is already initialized
+									// in a constructor
+									isAlreadyInitialized = true;
+								}
+							}
 						}
-						else if (primType.getPrimitiveTypeCode() == PrimitiveType.FLOAT) {
-							initializer = newFrag.getAST().newNumberLiteral("0.0f");
-						}
-						else if (primType.getPrimitiveTypeCode() == PrimitiveType.DOUBLE) {
-							initializer = newFrag.getAST().newNumberLiteral("0.0d");
-						}
-						else if (   primType.getPrimitiveTypeCode() == PrimitiveType.INT
-						         || primType.getPrimitiveTypeCode() == PrimitiveType.SHORT
-						         || primType.getPrimitiveTypeCode() == PrimitiveType.BYTE) {
-					 		initializer = newFrag.getAST().newNumberLiteral("0");
-						}
-						else if (primType.getPrimitiveTypeCode() == PrimitiveType.LONG) {
-							initializer = newFrag.getAST().newNumberLiteral("0L");
-						}
-						else {
-							continue; // TODO add assertion as this should not happen
-						}
+					} catch (JavaModelException e) {
+						// TODO: Trigger error
+						return false;
 					}
-					else if (fieldDeclType instanceof SimpleType) {
-						SimpleType simpType = (SimpleType)fieldDeclType;
-						assert simpType != null;
-						
-						// NOTE Not sure whether it is a good idea to handle strings unlike other objects
-						initializer = (simpType.getName().toString().equals("String")) 
-						            ? newFrag.getAST().newStringLiteral()
-						            : newFrag.getAST().newNullLiteral();
-					}
-					else {
-						continue; // TODO: not supported failure
+					catch (CoreException e) {
+						// TODO: Trigger error
+						return false;
 					}
 					
-					if (initializer != null) {
+					if (!isAlreadyInitialized) {
+						// Add initializer
+						Expression initializer = null;
+						if (fieldDeclType instanceof PrimitiveType) {
+							PrimitiveType primType = (PrimitiveType)fieldDeclType;
+							
+							if (primType.getPrimitiveTypeCode() == PrimitiveType.BOOLEAN) {
+								initializer = newFrag.getAST().newBooleanLiteral(false);
+							}
+							else if (primType.getPrimitiveTypeCode() == PrimitiveType.CHAR) {
+								CharacterLiteral charLit = newFrag.getAST().newCharacterLiteral(); 
+								charLit.setCharValue('\u0000');
+									
+								initializer = charLit;
+							}
+							else if (primType.getPrimitiveTypeCode() == PrimitiveType.FLOAT) {
+								initializer = newFrag.getAST().newNumberLiteral("0.0f");
+							}
+							else if (primType.getPrimitiveTypeCode() == PrimitiveType.DOUBLE) {
+								initializer = newFrag.getAST().newNumberLiteral("0.0d");
+							}
+							else if (   primType.getPrimitiveTypeCode() == PrimitiveType.INT
+							         || primType.getPrimitiveTypeCode() == PrimitiveType.SHORT
+							         || primType.getPrimitiveTypeCode() == PrimitiveType.BYTE) {
+						 		initializer = newFrag.getAST().newNumberLiteral("0");
+							}
+							else if (primType.getPrimitiveTypeCode() == PrimitiveType.LONG) {
+								initializer = newFrag.getAST().newNumberLiteral("0L");
+							}
+							else {
+								continue; // TODO add assertion as this should not happen
+							}
+						}
+						else if (fieldDeclType instanceof SimpleType) {
+							SimpleType simpType = (SimpleType)fieldDeclType;
+							assert simpType != null;
+							
+							// NOTE Not sure whether it is a good idea to handle strings unlike other objects
+							initializer = (simpType.getName().toString().equals("String")) 
+							            ? newFrag.getAST().newStringLiteral()
+							            : newFrag.getAST().newNullLiteral();
+						}
+						else {
+							continue; // TODO: not supported failure
+						}
+						
+						assert initializer != null;
+						
 						newFrag.setInitializer(initializer);
 					
 						TextEditGroup gd = new TextEditGroup("add initializer");
